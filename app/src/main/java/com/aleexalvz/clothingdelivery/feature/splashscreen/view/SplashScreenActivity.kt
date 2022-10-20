a1d0e9e047d5a7cabccf290ae5e04bcbc228ce60
Alex Alves <alex.alves_br@hotmail.com>
2022-10-20 19:43:31 -0300

    inittial commit

---
+++ app/src/main/java/com/aleexalvz/clothingdelivery/feature/splashscreen/view/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.aleexalvz.clothingdelivery.feature.splashscreen.view
+
+import android.app.Activity
+import android.os.Bundle
+import com.aleexalvz.clothingdelivery.R
+
+class SplashScreenActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
