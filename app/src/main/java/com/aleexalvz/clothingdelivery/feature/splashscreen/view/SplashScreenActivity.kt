64803c78c1f5a8618076759066d910f697d2349f
Alex Alves <alex.alves_br@hotmail.com>
2022-10-21 19:08:39 -0300

    create splash screen

--- app/src/main/java/com/aleexalvz/clothingdelivery/feature/splashscreen/view/SplashScreenActivity.kt
+++ app/src/main/java/com/aleexalvz/clothingdelivery/feature/splashscreen/view/SplashScreenActivity.kt
@@ -1,13 +1,34 @@
 package com.aleexalvz.clothingdelivery.feature.splashscreen.view
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.aleexalvz.clothingdelivery.R
+import com.aleexalvz.clothingdelivery.feature.login.view.LoginActivity
 
-class SplashScreenActivity: Activity() {
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        initSplashScreen()
+    }
+
+    private fun initSplashScreen() {
+        val countDownTimer = object : CountDownTimer(2000, 1000) {
+            override fun onTick(p0: Long) {}
+            override fun onFinish() {
+                navigateToLogin()
+            }
+        }
+        countDownTimer.start()
+    }
+
+    private fun navigateToLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
