c799cc1f85c7f65591346ad206883cee614e34ba
Alex Alves <alex.alves_br@hotmail.com>
2022-12-20 20:57:27 -0300

    add toolbar

--- app/src/main/java/com/aleexalvz/financeshelp/feature/home/HomeActivity.kt
+++ app/src/main/java/com/aleexalvz/financeshelp/feature/home/HomeActivity.kt
@@ -1,14 +1,17 @@
 package com.aleexalvz.financeshelp.feature.home
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.aleexalvz.financeshelp.R
-import com.aleexalvz.financeshelp.feature.appbar.AppBarFragment
 import com.aleexalvz.financeshelp.feature.home.lateststudies.LatestStudiesFragment
 import com.aleexalvz.financeshelp.feature.home.suggestions.SuggestionsFragment
 
-class HomeActivity: FragmentActivity() {
+class HomeActivity : FragmentActivity() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +21,28 @@ class HomeActivity: FragmentActivity() {
     }
 
     private fun findViews() {
-
+        toolbar = findViewById(R.id.home_top_app_bar)
     }
 
     private fun initViews() {
-        addFragment(R.id.home_app_bar_container, AppBarFragment())
+        configureToolBar()
         addFragment(R.id.latest_studies_container, LatestStudiesFragment())
         addFragment(R.id.next_course_suggestions_container, SuggestionsFragment())
+    }
+
+    private fun configureToolBar() {
+        toolbar.setNavigationOnClickListener {
+            Toast.makeText(this, "Abrir Menu", Toast.LENGTH_LONG).show()
+        }
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.top_app_bar_search -> {
+                    Toast.makeText(this, "Direcionar para tela de busca de cursos", Toast.LENGTH_LONG).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun addFragment(containerView: Int, fragment: Fragment) =
